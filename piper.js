341a55de1377aec88bae0b0f661ac9dce1e189b2
Zachary <zachjoliver@gmail.com>
2019-12-01 00:19:54 -0800

    Submit version

--- piper.js
+++ piper.js
@@ -1,8 +1,18 @@
 var countDeafRats = function (town) {
-  // Your code here
-  let defRat = '~O';
+  let defRat = 'O~';
+  let regRat = '~O';
+  let count = 0;
   for (let i = 0; i < town.length; i++) {
-    if 
+    if (town[i] === 'P') {
+      defRat = '~O';
+      regRat = 'O~';
+    }
+    if (town[i] + town[i + 1] === defRat) {
+      count += 1;
+      i+=1;
+    } else if (town[i] + town[i + 1] === regRat) {
+      i+=1;
+    }
   };
-  return 0;
+  return count;
 }
